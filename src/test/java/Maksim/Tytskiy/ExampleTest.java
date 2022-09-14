cbfd842ad26b5efde389bdc54e5f0bbe6dd89658
MaxBiggy1982 <maxbiggy1982@yandex.ru>
2022-09-14 20:21:08 +0300

    Init commit from console test

---
+++ src/test/java/Maksim/Tytskiy/ExampleTest.java
@@ -0,0 +1,12 @@
+package Maksim.Tytskiy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue( condition: 2 > 1);
+
+    }
+}
